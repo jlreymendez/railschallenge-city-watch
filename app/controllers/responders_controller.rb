650e8c9ce4ef5fd37db94408ee17dc79d5d3097e
José Luis Rey Méndez <jlreymendez@gmail.com>
2015-04-18 19:42:49 -0300

    Finished responders_create_test functionality.

--- app/controllers/responders_controller.rb
+++ app/controllers/responders_controller.rb
@@ -1,26 +1,40 @@
 class RespondersController < ApplicationController
+  rescue_from(ActionController::UnpermittedParameters) do |unpermitted_parameters|
+    message = { message: unpermitted_parameters.message }
+    render json: message.to_json, status: 422
+  end
+
   def create
-    responder = Responder.create(params.require(:responder).permit(:type, :name, :capacity))
+    ActionController::Parameters.action_on_unpermitted_parameters = :raise
+    strong_params = params.require(:responder).permit(:type, :name, :capacity)
+
+    responder = Responder.create(strong_params)
     @response = generate_response(responder)
 
     if responder.save
       render json: @response.to_json, status: 201
     else
-      render json: {}.to_json, status: 422
+      render json: @response.to_json, status: 422
     end
   end
 
   private
 
   def generate_response(responder = {})
-    {
-      responder: {
-        name: responder.name,
-        type: responder.type,
-        emergency_code: responder.emergency_code,
-        capacity: responder.capacity,
-        on_duty: responder.on_duty
+    if responder.save
+      {
+        responder: {
+          name: responder.name,
+          type: responder.type,
+          emergency_code: responder.emergency_code,
+          capacity: responder.capacity,
+          on_duty: responder.on_duty
+        }
       }
-    }
+    else
+      {
+        message: responder.errors
+      }
+    end
   end
 end
--- app/models/responder.rb
+++ app/models/responder.rb
@@ -1,5 +1,8 @@
 class Responder < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+  validates :capacity, presence: true, inclusion: { in: 1..5 }
+  validates :type, presence: true
+
   before_save :default_values
 
   def default_values
