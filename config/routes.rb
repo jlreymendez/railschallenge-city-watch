2505d201ba62b7abcfa05432cbfd8b3c7d9b8bbb
José Luis Rey Méndez <jlreymendez@gmail.com>
2015-04-21 23:43:01 -0300

    Simplifying routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  resources :emergencies, :responders, defaults: { format: 'json' }, only: :create
-
-  patch '/responders/:name' => 'responders#update', defaults: { format: 'json' }
-  get '/responders/:name' => 'responders#show', defaults: { format: 'json' }
-
-  patch 'emergencies/:code' => 'emergencies#update', defaults: { format: 'json' }
-  get 'emergencies/:code' => 'emergencies#show', defaults: { format: 'json' }
+  resources :responders, defaults: { format: 'json' }, only: [:create, :update, :show], param: :name
+  resources :emergencies, defaults: { format: 'json' }, only: [:create, :update, :show], param: :code
 end
